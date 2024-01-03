18e4211257d9b164d80d9e8c7deaaacb8da42075
Zuzanna Sosnowska <268483@student.pwr.edu.pl>
2024-01-03 23:26:41 +0100

    Poprawki

---
+++ Folder_na_kod/Metoda_numeryczna.py
@@ -0,0 +1,187 @@
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+
+def modifiedEulerMethod(function, initial_cond, parting, division=10 ** 5):
+    """
+    Funkcja rozwiązuje równanie różniczkowe zadane w postaci: x''(t) = lamb * f(t, x, x') z określonymi warunkami
+    początkowymi na danym przedziale np. na przedziale (0, 12.5) z następującymi warunkami: x(0) = 0,
+    x'(0) = 2 :param lamb: lambda :param function: funkcja zależąca od zmiennej; f(t, x, x') :param initial_cond:
+    warunek początkowy; wartości funkcji w punkcie parting[0] :param parting: przedział, dla którego otrzymamy
+    wartości funkcji po rozwiązaniu równania różniczkowego :param division: podział przedziału na division części
+    :return: funkcja zwraca dwie tablice - pierwsza z nich to podział odcinka parting na division części,
+    druga tablica z kolei zwraca wartości szukanej funkcji dla odpowiadających jej punktów z pierwszej tablicy
+    """
+    if parting[0] <= initial_cond[0][0] <= parting[1]:
+        left = initial_cond[0][0] - parting[0]
+        right = parting[1] - initial_cond[0][0]
+        left_div = int(left / (left + right) * division)
+        right_div = int(right / (left + right) * division)
+        x_list = left_div * [0]
+        y_list = left_div * [0]
+        w_list = left_div * [0]
+        w = initial_cond[0][0]
+        c = left_div - 1
+        x_list.append(initial_cond[0][1])
+        y_list.append(initial_cond[1][1])
+        w_list.append(w)
+        if left_div != 0:
+            left = left / left_div
+            while c >= 0:
+                x_list[c] = x_list[c + 1] - left * y_list[c + 1]
+                y_list[c] = y_list[c + 1] - left * function(w, x_list[c + 1], y_list[c + 1])
+                w -= left
+                w_list[c] = w
+                c -= 1
+        if right_div != 0:
+            w = initial_cond[0][0]
+            right = right / right_div
+            while w <= parting[1] - right:
+                x_list.append(x_list[-1] + right * y_list[-1])
+                y_list.append(y_list[-1] + right * function(w, x_list[-2], y_list[-1]))
+                w += right
+                w_list.append(w)
+        return w_list, x_list
+
+
+def shootingMethod(function, boundary_cond, parting, a_part, division=10 ** 5 +1, epsilon=0.001):
+    """
+    :param function:
+    :param boundary_cond:
+    :param parting:
+    :param division:
+    :param epsilon:
+    :return:
+    """
+    a = modifiedEulerMethod(function, [boundary_cond[0], (boundary_cond[0][0], a_part[0])], parting, division)[1][-1] - \
+        boundary_cond[1][1]
+    b = modifiedEulerMethod(function, [boundary_cond[0], (boundary_cond[0][0], a_part[1])], parting, division)[1][-1] - \
+        boundary_cond[1][1]
+    if a * b >= 0:
+        raise Exception("coś nie tak z przedziałem XD")
+    while True:
+        c = (a_part[1] + a_part[0]) / 2
+        p = modifiedEulerMethod(function, [boundary_cond[0], (boundary_cond[0][0], c)], parting, division)
+        q = p[1][-1] - boundary_cond[1][1]
+        if abs(q) <= epsilon:
+            return c
+        else:
+            if a * q > 0:
+                a_part = (c, a_part[1])
+            elif a * q < 0:
+                a_part = (a_part[0], c)
+
+
+def analyticMethodSODE(list_of_coefs, initial_cond, parting, div=10**5):
+    """
+    Funkcja rozwiązuje analitycznie równanie różniczkowe jednorodne zadane w postaci: a * y''(t) + b * y'(t) + c * y(t) = 0
+    :param list_of_coefs: współczynniki w kolejności: b, c
+    :param initial_cond: warunki początkowe zadane w postaci: [(t0, y(t0) = y1), (t0, y'(t0) = y2)]
+    :param parting: przedział, dla jakiego chcemy otrzymać wartości funkcji
+    :return:
+    """
+    if initial_cond[0][0] != initial_cond[1][0]:
+        raise ValueError('Pochodna jest w innym punkcie!')
+    a, b, c = list_of_coefs
+    if a == 0:
+        raise ValueError('Źle dobrane współczynniki! Współczynnik przy drugiej pochodnej musi być różny od zera!')
+    values_list = []
+    arguments_list = []
+    delta_r = b ** 2 - 4 * c
+    if delta_r == 0:
+        r = -b/(2*a)
+        expon = math.exp(r*initial_cond[0][0])
+        A = np.array([[expon, initial_cond[1][0] * expon], [r * expon, r * initial_cond[1][0] * expon + expon]])
+        B = np.array([initial_cond[0][1], initial_cond[1][1]])
+        C_1, C_2 = np.linalg.inv(A).dot(B)
+        t = parting[0]
+        delta_t = (parting[1] - parting[0])/div
+        for i in range(div):
+            values_list.append(C_1*math.exp(r*t) + C_2*math.exp(r*t)*t)
+            arguments_list.append(t)
+            t += delta_t
+        return arguments_list, values_list
+    elif delta_r > 0:
+        r1 = (- math.sqrt(delta_r) - b)/(2 * a)
+        r2 = (math.sqrt(delta_r) - b)/(2 * a)
+        exp1 = math.exp(r1 * initial_cond[0][0])
+        exp2 = math.exp(r2 * initial_cond[1][0])
+        A = np.array([[exp1, exp2], [r1 * exp1, r2 * exp2]])
+        B = np.array([initial_cond[0][1], initial_cond[1][1]])
+        C_1, C_2 = np.linalg.inv(A).dot(B)
+        t = parting[0]
+        delta_t = (parting[1] - parting[0]) / div
+        for i in range(div):
+            values_list.append(C_1 * math.exp(r1 * t) + C_2 * math.exp(r2 * t))
+            arguments_list.append(t)
+            t += delta_t
+        return arguments_list, values_list
+    else:
+        re = -b/(2*a)
+        im = math.sqrt(-delta_r)/(2*a)
+        exp = math.exp(re * initial_cond[0][0])
+        sin = math.sin(im * initial_cond[0][0])
+        cos = math.cos(im * initial_cond[0][0])
+        A = np.array([[exp * sin, exp * cos], [re*exp*sin + exp*cos*im, re*exp*cos - exp*sin*im]])
+        B = np.array([initial_cond[0][1], initial_cond[1][1]])
+        C_1, C_2 = np.linalg.inv(A).dot(B)
+        t = parting[0]
+        delta_t = (parting[1] - parting[0]) / div
+        for i in range(div):
+            values_list.append(math.exp(re*t) * (C_1 * math.sin(im*t) + C_2 * math.cos(im*t)))
+            arguments_list.append(t)
+            t += delta_t
+        return arguments_list, values_list
+
+
+def funP(x, P, Y, n=4.0, l=10.0):
+    return - (math.pi * n / l) ** 2 * P
+
+
+def funQ(t, Q, Y, n=2.0, l=10.0, c=2.0):
+    return - (math.pi * n / l) ** 2 * c ** 2 * Q
+
+
+def f_func(x):
+    return -0.1 * (x - 5) ** 2 + 2.5
+
+
+def g_func(x):
+    return -0.1 * (x - 5) ** 2 + 2.5
+
+
+def initialConditionsToQ(x, f_func, g_func, fun_p_lists, eps=0.0001):
+    for i in range(len(fun_p_lists[0])):
+        if abs(fun_p_lists[0][i] - x) <= eps:
+            a = fun_p_lists[1][i]
+            return [(0, f_func(x) / a), (0, g_func(x) / a)]
+    raise 'No value for given argument'
+
+
+if __name__ == '__main__':
+    n = 3
+    l = 10.0
+    c = 1.0
+    d = shootingMethod(lambda w, x, y: funP(w, x, y, n, l), [(0.0, 0.0), (l, 0.0)], (0.0, l), (-3.0, 2.0))
+    P = modifiedEulerMethod(lambda w, x, y: funQ(w, x, y, n, l, 1.0), [(0.0, 0.0), (0.0, d)], (0.0, l))
+
+    P_an = analyticMethodSODE([1, 0, (math.pi * n / l) ** 2], [(0.0, 0.0), (0.0, d)], (0.0, l))
+
+    # plt.plot(P[0], P[1])
+    # plt.plot(P_an[0], P_an[1])
+    # plt.show()
+
+    Q = modifiedEulerMethod(lambda w, x, y: funQ(w, x, y, n, l, 1.0), initialConditionsToQ(3, f_func, g_func, P),
+                            (0.0, l))
+
+    Q_an = analyticMethodSODE([1, 0, (math.pi * n / l) ** 2 * c ** 2], initialConditionsToQ(3, f_func, g_func, P), (0.0, l))
+
+    X = np.array(P[1]) * Q[1][7000]
+    X_an = np.array(P_an[1]) * Q_an[1][7000]
+
+    plt.plot(P[0], X)
+    plt.plot(P_an[0], X_an)
+    plt.show()
--- Folder_na_kod/gif_animation.py
+++ Folder_na_kod/gif_animation.py
@@ -2,31 +2,33 @@ import matplotlib.pyplot as plt
 import numpy as np
 from PIL import Image, ImageSequence
 from io import BytesIO
-import Shooting_Method as sm
+import Metoda_numeryczna as sm
 import math
 
-P = sm.shootingMethod(lambda w, x, y: sm.funP(w, x, y, math.pi / 1), [(0.0, 0.0), (10.0, 0.0)], (0.0, 10.0), (-3.0, 2.0))
+n = 2
+l = 10.0
+c = 1.0
 
-# p = modifiedEulerMethod(lambda w, x, y: funP(w, x, y, math.pi), [(0.0, 0.0), (0.0, 1.0)], (0.0, 10.0))
-
-Q = sm.modifiedEulerMethod(lambda w, x, y: sm.funQ(w, x, y, math.pi / 1, 1.0), sm.initialConditionsToQ(3, sm.f_func, sm.g_func, P),
-                        (0.0, 10.0))
+d = sm.shootingMethod(lambda w, x, y: sm.funP(w, x, y, n, l), [(0.0, 0.0), (l, 0.0)], (0.0, l), (-3.0, 2.0))
+P = sm.modifiedEulerMethod(lambda w, x, y: sm.funQ(w, x, y, n, l, 1.0), [(0.0, 0.0), (l, c)], (0.0, l))
+Q = sm.modifiedEulerMethod(lambda w, x, y: sm.funQ(w, x, y, n, l, c), sm.initialConditionsToQ(3, sm.f_func, sm.g_func, P),
+    (0.0, l))
 
 
 # Function to create a plot and return the image as bytes
-def create_plot(values, constant):
+def create_plot(values, constant, P, Q):
+    m = np.max(np.abs(P[1])) * np.max(np.abs(Q[1])) * 1.2
     x = np.array(P[0])
     y = values * constant
 
     plt.figure(figsize=(6, 4))
     plt.plot(x, y)
-    plt.xlabel('Index')
-    plt.ylabel('Values * Constant')
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$u_{1}(t,x)$')
     plt.grid(True)
-    plt.tight_layout()
 
     # Set y-axis limits to maintain consistency
-    plt.ylim(-10000, 10000)  # Adjust the limits based on your data range
+    plt.ylim(-m, m)  # Adjust the limits based on your data range
 
     # Save the plot as an image in memory
     image_bytes = BytesIO()
@@ -42,15 +44,15 @@ values = np.array(P[1])
 
 # Example list of constants
 a = []
-n = 30
-for i in range(n):
-    a.append(Q[1][len(Q[1])*i//n])
+b = 30
+for i in range(b):
+    a.append(Q[1][len(Q[1])*i//b])
 constants_list = np.array(a)
 
 # Create frames for each constant
 frames = []
 for constant in constants_list:
-    frame = create_plot(values, constant)
+    frame = create_plot(values, constant, P, Q)
     frames.append(Image.open(BytesIO(frame)))
 
 # Create a GIF from the frames
@@ -66,7 +68,7 @@ frames[0].save(
 
 # Save or display the GIF as needed
 # For example, to save the GIF to a file:
-with open('multiplication_animation.gif', 'wb') as f:
+with open(f'struna_gitarowa_n={n}.gif', 'wb') as f:
     f.write(gif_bytes.getvalue())
 
 print("Animated GIF created in memory.")
